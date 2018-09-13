a4cd44813745bc5d7104d7735efb6b029cd42c3b
mrkirby153 <mr.austinwhyte@gmail.com>
2018-09-13 10:02:19 -0700

    Add support for multiple formats of message IDs

--- src/main/java/com/mrkirby153/tgabot/KUtils.kt
+++ src/main/java/com/mrkirby153/tgabot/KUtils.kt
@@ -7,9 +7,12 @@ import net.dv8tion.jda.core.entities.Message
 import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.requests.RestAction
 import java.util.concurrent.TimeUnit
+import java.util.regex.Pattern
 
 const val RED_CROSS = "❌"
 const val GREEN_CHECK = "✅"
+val JUMP_LINK_PATTERN: Pattern = Pattern.compile(
+        "https?://(canary\\.|ptb\\.)?discordapp.com/channels/(\\d{17,18})/(\\d{17,18})/(\\d{17,18})")
 
 fun Message.removeReaction(user: User, reaction: String): RestAction<Void> {
     val r = this.reactions.first { it.reactionEmote.name == reaction }
@@ -34,14 +37,55 @@ fun findEmoteById(emote: String): Emote? {
 private val msgCache = CacheBuilder.newBuilder().expireAfterWrite(30,
         TimeUnit.SECONDS).build<String, String>()
 
+private fun Guild.getMessage(channel: String, id: String): Message? {
+    return try {
+        this.getTextChannelById(channel)?.getMessageById(id)?.complete()
+    } catch (e: Exception) {
+        null
+    }
+}
+
 fun Guild.findMessageById(id: String): Message? {
     Bot.logger.debug("Finding message with ID of $id in $this")
 
+    if (id.matches(Regex("\\d{17,18}-\\d{17,18}"))) {
+        Bot.logger.debug("Matches channelid-messageid. Parsing...")
+        val split = id.split("-")
+        val channel = split[0]
+        val msg = split[1]
+
+        val m = getMessage(channel, msg)
+        if (m != null) {
+            msgCache.put(msg, channel)
+            return m
+        }
+    }
+
+    val matcher = JUMP_LINK_PATTERN.matcher(id)
+    if (matcher.find()) {
+        Bot.logger.debug("Matches jump link... Parsing")
+        val channel = matcher.group(3)
+        val msg = matcher.group(4)
+        try {
+            return getTextChannelById(channel)?.getMessageById(msg)?.complete()
+        } catch (e: Exception) {
+            // Ignore
+        }
+    }
+
+
     val cached = msgCache.getIfPresent(id)
     if (cached != null) {
-        val m = this.getTextChannelById(cached)?.getMessageById(id)?.complete() ?: return null
-        msgCache.put(id, m.channel.id)
-        return m
+        val m = getMessage(cached, id)
+        if (m != null) {
+            msgCache.put(id, m.channel.id)
+            return m
+        }
+    }
+    // All other methods have failed. Lets try iterating
+    if (!id.matches(Regex("\\d{17,18}"))) {
+        Bot.logger.debug("ID passed is not a snowflake.")
+        return null
     }
     this.textChannels.forEach {
         try {
